44a3d63d9322a1059478c06b352fc77dcfc55843
jiandankuanggong <74077677+1460293896@users.noreply.github.com>
2022-04-17 15:44:09 +0800

    Create 1学习5.2.py
    学习5.2python

---
+++ 1学习5.2.py
@@ -0,0 +1,10 @@
+num =[2,3,4,6]
+good =["我是密码","我是中国人"]
+good1 =["33333333",["我是中国人2","我是中国人3"]]
+python = ['优势1']
+konglist =[]      #是空的表
+print(konglist)
+ko= list(range(2,21,2))   #用range数据
+print(ko)
+del ko               #除网站
+print(ko)
